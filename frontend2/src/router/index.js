e863366503be0fb0fb887e48ff93911a506f8028
Alexey Lapin <a.lapin03@gmail.com>
2025-02-18 19:04:10 +0300

    fix: register route

--- frontend2/src/router/index.js
+++ frontend2/src/router/index.js
@@ -1,30 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+import AdminPage from '@/components/AdminPage.vue';
+import AssessorPage from '@/components/AssessorPage.vue';
 import { useAuthStore } from '@/stores/auth';
 
 const routes = [
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('@/components/Login.vue')
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import('@/components/AdminPage.vue'),
-    meta: { 
-      requiresAuth: true,
-      roles: ['ADMIN']
-    }
-  },
-  {
-    path: '/assessor',
-    name: 'Assessor',
-    component: () => import('@/components/AssessorPage.vue'),
-    meta: { 
-      requiresAuth: true,
-      roles: ['ADMIN', 'ASSESSOR']
-    }
-  },
   {
     path: '/',
     redirect: to => {
@@ -32,12 +13,41 @@ const routes = [
       if (!authStore.isAuthenticated) return '/login';
       return authStore.user?.role === 'ADMIN' ? '/admin' : '/assessor';
     }
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/components/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/components/Register.vue')
+  },
+  {
+    path: '/assessor',
+    name: 'Assessor',
+    component: () => import('@/components/AssessorPage.vue'),
+    meta: {
+      requiresAuth: true,
+      roles: ['ADMIN', 'ASSESSOR']
+    }
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import('@/components/AdminPage.vue'),
+    meta: {
+      requiresAuth: true,
+      roles: ['ADMIN']
+    }
+  },
+
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
